a6f57d81e686a361477c3898a8482fda7b705384
Jamie Desktop <jamiebclark@gmail.com>
2017-07-05 15:33:42 -0400

    Added comments

--- Controller/Component/VendorAssetAutoLoadComponent.php
+++ Controller/Component/VendorAssetAutoLoadComponent.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Copies assets from vendor folders into the webroot folder so they are publicly accessible
+ *
+ **/
+
 App::uses('Folder', 'Utility');
 App::uses('File', 'Utility');
 App::uses('AssetFile', 'CakeAssets.Utility');
@@ -10,15 +15,33 @@ class VendorAssetAutoLoadComponent extends Component {
 	private $_types = ['css', 'js', 'files'];
 
 	public function __construct(ComponentCollection $collection, $settings = []) {
+		// Configuration
 		$default = [
+			/**
+			 * Include Asset files
+			 *
+			 * Use the format ['filename'] or ['src/sourceFileName' => 'dst/destinationFileName']
+			 **/
+			// Any CSS files
 			'css' => [],
+			// Any JS files
 			'js' => [],
+			// Any additional files
 			'files' => [],
 
+			/**
+			 * Add the sources where to look
+			 *
+			 **/
 			'src' => [
-				ROOT . DS . 'node_modules' . DS,
-				APP . 'Vendor' . DS,
+				ROOT . DS . 'node_modules' . DS,	// Node
+				APP . 'Vendor' . DS,				// Composer
 			],
+
+			/**
+			 * The root destionation
+			 * It will also append $ASSET_TYPE/vendor to the destination directory
+			 **/
 			'dst' => APP .  WEBROOT_DIR,
 		];
 		$this->settings = array_merge($default, (array) $settings);
@@ -29,6 +52,11 @@ class VendorAssetAutoLoadComponent extends Component {
 		$this->autoload();
 	}
 
+/**
+ * Looks through the source folders for changes and copies new files and folders
+ *
+ * @return null;
+ **/
 	public function autoload() {
 		$files = [];
 		foreach ($this->_types as $assetType) {
@@ -57,6 +85,15 @@ class VendorAssetAutoLoadComponent extends Component {
 		}
 	}
 
+/**
+ * Copies an entire directory into a destination directory
+ * Only copies a file if it is newer
+ *
+ * @param string $srcFolder The source folder
+ * @param string $dstFolder The destination folder
+ * @param bool $force If true, then copy the files no matter what
+ * @return null;
+ **/
 	private function copyDir($srcFolder, $dstFolder, $force = false) {
 		$dir = new Folder($srcFolder);
 		$files = $dir->read(false);
@@ -80,6 +117,14 @@ class VendorAssetAutoLoadComponent extends Component {
 		}
 	}
 
+/**
+ * Copies an individual file to a new location
+ *
+ * @param string $srcFile The source path
+ * @param string $dstFile The destination path
+ * @param bool $force If true, copy no matter what
+ * @return bool|null True if success, false if failure, null if no copy
+ **/
 	private function copyFile($srcFile, $dstFile, $force = false) {
 		if ($force || !is_file($dstFile) || filesize($srcFile) != filesize($dstFile)) {
 			return AssetFile::copy($srcFile, $dstFile);
@@ -88,6 +133,13 @@ class VendorAssetAutoLoadComponent extends Component {
 		}
 	}
 
+/**
+ * Returns the destination directory based on the asset type
+ * 
+ * @param string $assetType The type of asset being referenced
+ * @param string $filePath An additional file path to append to the directory
+ * @return string An updated destination directory path
+ **/
 	private function getDstDir($assetType, $filePath = null) {
 		return AssetFile::getPath([
 			$this->settings['dst'],
